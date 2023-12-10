7c0819fed5bc7fa55fb2264352367cfe0c45ebec
厚轩 <yongfei@cai-inc.com>
2023-12-10 10:46:15 +0800

    feat: lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -34,5 +34,13 @@ module.exports = {
       { allowConstantExport: true },
     ],
     '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-unsafe-call': 'off',
+    '@typescript-eslint/no-unsafe-assignment': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/no-misused-promises': 'off',
+    '@typescript-eslint/no-unsafe-member-access': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
   },
 }
--- .husky/lint-staged.config.js
+++
@@ -1,7 +0,0 @@
-// .husky/lint-staged.config.js
-module.exports = {
-  'packages/**.{js,jsx,ts,tsx}': ['eslint --fix', 'prettier --write'],
-  'package.json': ['prettier --write'],
-  'packages/***.vue': ['eslint --fix', 'prettier --write', 'stylelint --fix'],
-  'packages/***.{scss,less,styl,html}': ['stylelint --fix', 'prettier --write'],
-};
